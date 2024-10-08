cca0597feb0a1246db7d2b69dfdc030029a6dc5c
Chun Yui Kasper Ku <kasperku.hk@gmail.com>
2024-10-07 21:02:07 -0700

    Created class PokerTracker and provided class level documentation.
    Main class now creates an instance of the PokerTracker class

---
+++ src/main/ui/PokerTracker.java
@@ -0,0 +1,23 @@
+package ui;
+
+import model.PokerGame;
+import java.util.List;
+
+// PokerTracker application
+public class PokerTracker {
+
+    private List<PokerGame> gameHistory;
+
+    // EFFECTS: run the PokerTracker application
+    public PokerTracker(){
+        runPokerTrackerInput();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: process user input
+
+    public void runPokerTrackerInput(){
+        // stub
+    }
+    
+}
