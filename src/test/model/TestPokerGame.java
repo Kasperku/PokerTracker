88cf21c3de5384f860476118953dc31a99c851dc
Chun Yui Kasper Ku <kasperku.hk@gmail.com>
2024-10-08 03:25:23 -0700

    Added Test for TestPokerGame. Code coverage is now 100%

--- src/test/model/TestPokerGame.java
+++ src/test/model/TestPokerGame.java
@@ -2,6 +2,7 @@ package model;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,4 +22,52 @@ public class TestPokerGame extends BaseTest {
         assertEquals(testPokerGame1.getAmount(), 3000);
         assertEquals(testPokerGame1.getCards(), testCards1);
     } 
+
+    @Test
+    void testGetHasWon(){
+        assertTrue(testPokerGame1.getHasWon());
+        assertFalse(testPokerGame2.getHasWon());
+    }
+
+    @Test
+    void testGetAmount(){
+        assertEquals(testPokerGame1.getAmount(), 3000);
+        assertEquals(testPokerGame2.getAmount(), -200);
+    }
+
+    @Test
+    void testGetCards(){
+        assertEquals(testCards1, testPokerGame1.getCards());
+        assertEquals(testCards2, testPokerGame2.getCards());
+    }
+
+    @Test
+    void testSetHasWon(){
+        testPokerGame1.setHasWon(false);
+        assertFalse(testPokerGame1.getHasWon());
+
+        testPokerGame2.setHasWon(true);
+        assertTrue(testPokerGame2.getHasWon());
+    }
+
+    @Test
+    void testSetAmount() {
+        testPokerGame1.setAmount(1000);
+        assertEquals(1000, testPokerGame1.getAmount());
+
+        testPokerGame4.setAmount(-300);
+        assertEquals(-300, testPokerGame1.getAmount());
+    }
+
+    @Test
+    void testSetCards() {
+        List<Card> testCards = new ArrayList<>();
+        testCards.add(new Card("2", "Clubs"));
+        testCards.add(new Card("2", "Spades"));
+
+        testPokerGame2.setCards(testCards);
+        assertEquals(testCards, testPokerGame2.getCards());
+    }
+
+
 }
