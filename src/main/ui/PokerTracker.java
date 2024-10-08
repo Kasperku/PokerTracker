af48cbae7e14e103f18aa4c23da61a9066b78c2c
Chun Yui Kasper Ku <kasperku.hk@gmail.com>
2024-10-07 23:35:30 -0700

    Added method stub and update field for PokerTracker Class

--- src/main/ui/PokerTracker.java
+++ src/main/ui/PokerTracker.java
@@ -2,11 +2,13 @@ package ui;
 
 import model.PokerGame;
 import java.util.List;
+import java.util.Scanner;
 
 // PokerTracker application
 public class PokerTracker {
 
     private List<PokerGame> gameHistory;
+    private Scanner input;
 
     // EFFECTS: run the PokerTracker application
     public PokerTracker(){
@@ -31,5 +33,53 @@ public class PokerTracker {
         // stub
     }
 
+    // MODIFIES: this
+    // EFFECTS: initializes gameHistory
+    private void init() {
+        // stub
+    }
+
     
+
+    // MODIFIES: this
+    // EFFECTS: adds a pokerGame to gameHistory
+    private void addNewPokerGame(){
+        // stub
+    }
+
+    // EFFECTS: Display a list of logged poker games 
+    private void viewPokerGames(){
+        // stub
+    }
+
+    // EFFECTS: Display game statistics
+    private void checkStatsSummary(){
+        // stub
+    }
+
+
+
+    // MODIFIES: this
+    // EFFECTS: Edit an existing poker game
+    private void editPokerGame(){
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: Delete an existing poker game
+    private void delPokerGame(){
+        // stub
+    }
+
+
+
+    // MODIFIES: this
+    // EFFECTS: sort the games in gameHistory by given criteria
+    private void sortGames(String crit){
+        // stub
+    }
+
+
+
+
 }
