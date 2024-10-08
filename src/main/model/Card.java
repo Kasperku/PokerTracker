77cbf8bc5397c1c2b3ff575986f9f44bbcc73a35
Chun Yui Kasper Ku <kasperku.hk@gmail.com>
2024-10-07 20:35:26 -0700

    Provided documentation for Class PokerGame and Card

--- src/main/model/Card.java
+++ src/main/model/Card.java
@@ -1,6 +1,15 @@
 package model;
 
 // Represents a card with suit and rank
+
 public class Card {
 
+    /*
+     * REQUIRES: cards must be of length 2, card cannot be duplicate
+     * EFFECTS: new PokerGame with outcome, amount won, and cards held.
+     */
+
+     public Card() {
+        //stub
+    }
 }
--- src/main/model/PokerGame.java
+++ src/main/model/PokerGame.java
@@ -1,22 +1,15 @@
 package model;
 
-import java.util.List;
-
 // Represents a single poker game having a outcome, amt won/loss, cards held.
 
 public class PokerGame {
-    private boolean haswon;
-    int amount;
-    private List<Card> cards;
 
     /*
-     * REQUIRES: cards must be of length 2
+     * REQUIRES: cards must be of length 2, card cannot be duplicate
      * EFFECTS: new PokerGame with outcome, amount won, and cards held.
      */
 
-    public PokerGame(boolean haswon, int amount, List<Card> cards) {
-        this.haswon = haswon;
-        this.amount = amount;
-        this.cards = cards;
+    public PokerGame() {
+        //stub
     }
 }
