677741887b682fc14f73b33e924e54aee2e73376
Chun Yui Kasper Ku <kasperku.hk@gmail.com>
2024-10-08 00:31:29 -0700

    Added PokerManager Class

---
+++ src/main/model/PokerManager.java
@@ -0,0 +1,38 @@
+package model;
+
+import java.util.List;
+import java.util.Map;
+
+// represents a PokerManager for calculating statistics and sorting of games played
+public class PokerManager {
+    
+    // EFFECTS: returns winrate
+    public int calculateWinRate(List<PokerGame> pokergames){
+        // stub
+        return 0;
+    }
+
+    // EFFECTS: returns total winnings, - if loss 
+    public int calculateWinnings(List<PokerGame> pokergames){
+        // stub
+        return 0;
+    }
+
+    // EFFECTS: analyze which hand lead to most losses 
+    public Map<String, Integer> analyzeLosingHands(List<PokerGame> pokergames){
+        // stub
+        return null;
+    }
+
+    // EFFECTS: sort List<PokerGame> by amount won
+    public List<PokerGame> sortByAmountWon(List<PokerGame> pokergames){
+        // stub
+        return null;
+    }
+
+    // EFFECTS: sort List<PokerGame> by win/loss
+    public List<PokerGame> sortByWinLoss(List<PokerGame> pokergames){
+        // stub
+        return null;
+    }
+}
