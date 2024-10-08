a7c2bfe284e65af5d3a42cbf39f4931bdeef7ba9
Chun Yui Kasper Ku <kasperku.hk@gmail.com>
2024-10-07 21:28:34 -0700

    Added method stubs to the PokerGame Class

--- src/main/model/PokerGame.java
+++ src/main/model/PokerGame.java
@@ -11,11 +11,38 @@ public class PokerGame {
     private List<Card> cards; // a list consisting of 2 cards
 
     /*
-     * REQUIRES: cards must be of length 2, card cannot be duplicate
+     * REQUIRES: cards must be of length 2, cards cannot contain duplicate cards
      * EFFECTS: new PokerGame with outcome, amount won, and cards held.
      */
 
     public PokerGame() {
+        //stub
+    }
+
+    public boolean getHasWon(){
+        // stub
+        return false;
+    }
+
+    public int getAmount(){
+        // stub
+        return 0;
+    }
+
+    public List<Card> getCards(){
+        //stub
+        return null;
+    }
+
+    public void setHasWon(boolean hasWon){
+        //stub
+    }
+
+    public void setAmount(int amount){
+        //stub
+    }
+
+    public void setCards(List<Card> cards){
         //stub
     }
 }
