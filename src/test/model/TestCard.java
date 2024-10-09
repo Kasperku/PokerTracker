d0f278a451e69ce1a90588e29f0e1c0099789b19
Chun Yui Kasper Ku <kasperku.hk@gmail.com>
2024-10-08 22:46:19 -0700

    Added more test to increase coverage

--- src/test/model/TestCard.java
+++ src/test/model/TestCard.java
@@ -1,12 +1,15 @@
 package model;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TestCard extends BaseTest {
 
+    private Card testCard5;
 
     @BeforeEach
     void runBefore() {
@@ -39,5 +42,39 @@ public class TestCard extends BaseTest {
         testCardA.setSuit("Diamonds");
         assertEquals("Diamonds", testCardA.getSuit()); 
     }
+
+    @Test
+    void testEqualSameObject(){
+        assertTrue(testCard2.equals(testCard2));
+    }
+
+    @Test
+    void testEqualSameCard(){
+        testCard5 = new Card("2", "Diamonds");
+        assertTrue(testCard2.equals(testCard5));
+    }
+
+    @Test
+    void testNotEqualDiffCard() {
+        assertFalse(testCard2.equals(testCard3));
+    }
+
+    @Test
+    void testNotEqualDiffObject() {
+        assertFalse(testCard7.equals(""));
+    }
+
+    @Test
+    void testNotEqualDiffSuit() {
+        Card cardAD = new Card("A", "Diamonds");
+        assertFalse(testCardA.equals(cardAD)); 
+    }
+
+    @Test
+    void testNotEqualDiffRank() {
+        Card cardKH = new Card("K", "Hearts");
+        assertFalse(testCardA.equals(cardKH));
+    }
+
 }
 
