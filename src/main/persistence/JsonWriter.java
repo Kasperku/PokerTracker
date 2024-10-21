c8f958eae8c4e40a0796f0f1b83a027e7fab3986
Chun Yui Kasper Ku <kasperku.hk@gmail.com>
2024-10-21 01:10:20 -0700

    wrote method stubs for JsonWriter

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -4,9 +4,44 @@ import model.PokerGame;
 import model.Card;
 import org.json.JSONObject;
 
-
 import java.io.*;
 
 public class JsonWriter {
+    // code adapted from The University Of British Columbia,
+    // CPSC210 Winter 2024, jsonSerializationDemo
 
+    private static final int TAB = 4;
+    private PrintWriter writer;
+    private String destination;
+
+    // EFFECTS: constructs writer to write to destination file
+    public JsonWriter(String destination) {
+        this.destination = destination;
+    }
+
+    // MODIFIES: this
+    // EFFECTS: opens writer; throws FileNotFoundException if destination file
+    // cannot
+    // be opened for writing
+    public void open() throws FileNotFoundException {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes JSON representation of PokerGame to file
+    public void write(PokerGame pg) {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: closes writer
+    public void close() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: writes string to file
+    private void saveToFile(String json) {
+        // stub
+    }
 }
