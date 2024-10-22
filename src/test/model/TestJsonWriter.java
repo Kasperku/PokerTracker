71ee48e270ffc7defc9b4acc313adaa32e7f789a
Chun Yui Kasper Ku <kasperku.hk@gmail.com>
2024-10-21 19:29:38 -0700

    Implemented test methods for TestJsonWriter

--- src/test/model/TestJsonWriter.java
+++ src/test/model/TestJsonWriter.java
@@ -2,8 +2,10 @@ package model;
 
 import model.Card;
 import model.PokerGame;
+import persistence.JsonReader;
 import persistence.JsonWriter;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -13,17 +15,20 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestJsonWriter {
+    List<Card> hand;
+
+    @BeforeEach
+    void runBefore(){
+        Card cardK = new Card("K", "Hearts");
+        Card cardA = new Card("A", "Diamonds");
+        hand = new ArrayList<>();
+        hand.add(cardK);
+        hand.add(cardA);
+    }
 
     @Test
     void testWriterInvalidFile() {
         try {
-
-            Card cardK = new Card("K", "Hearts");
-            Card cardA = new Card("A", "Diamonds");
-            List<Card> hand = new ArrayList<>();
-            hand.add(cardK);
-            hand.add(cardA);
-
             PokerGame pg = new PokerGame(false, 999, hand);
 
             JsonWriter writer = new JsonWriter("./data/my\0illegal:fileName.json");
@@ -31,6 +36,52 @@ class TestJsonWriter {
             fail("IOException was expected");
         } catch (IOException e) {
             // pass
+        }
+    }
+
+    @Test
+    void testWriterEmptyPokerGame() {
+        try {
+            ArrayList<Card> dummy = new ArrayList<>();
+
+            PokerGame pg = new PokerGame(false, 0, dummy);
+            JsonWriter writer = new JsonWriter("./data/testWriterEmptyPokerGame.json");
+            writer.open();
+            writer.write(pg);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterEmptyPokerGame.json");
+            pg = reader.read();
+            assertFalse(pg.getHasWon());
+            assertEquals(0, pg.getAmount());
+            assertEquals(0, pg.getCards().size());  
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
+        }
+    }
+
+    @Test
+    void testWriterGeneralPokerGame() {
+        try {
+            PokerGame pg = new PokerGame(true, 999, hand);
+            JsonWriter writer = new JsonWriter("./data/testWriterGeneralPokerGame.json");
+            writer.open();
+            writer.write(pg);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterGeneralPokerGame.json");
+            pg = reader.read();
+            assertTrue(pg.getHasWon());
+            assertEquals(999, pg.getAmount());
+            List<Card> hand = pg.getCards();
+            assertEquals(2, hand.size());
+            assertEquals("K", hand.get(1).getRank());
+            assertEquals("Hearts", hand.get(1).getSuit());
+            assertEquals("A", hand.get(0).getRank());
+            assertEquals("Spades", hand.get(0).getSuit());
+
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
         }
     }
 }
