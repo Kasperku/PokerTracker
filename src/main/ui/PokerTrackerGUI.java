c3826809b4030c36e503982af41b0d51ef5a34b2
Chun Yui Kasper Ku <kasperku.hk@gmail.com>
2024-11-11 17:01:55 -0800

    Added a PokerTrackerGUI class, with a constructor stub

---
+++ src/main/ui/PokerTrackerGUI.java
@@ -0,0 +1,16 @@
+package ui;
+
+import javax.swing.JFrame;
+/*
+ * Represents the main window in which the pokerTracker
+ * is displayed
+ */
+
+public class PokerTrackerGUI extends JFrame {
+
+    // effects: sets up window in which pokerTracker
+    // will be displayed
+    public PokerTrackerGUI() {
+        // stub
+    }
+}
